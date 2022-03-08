0f7b05df18dacaf522062dc1d60538cb553639e9
spacmercedes <79169837+spacmercedes@users.noreply.github.com>
2022-03-08 17:19:47 +0200

    HomeworkLab2

---
+++ ComputerLab.java
@@ -0,0 +1,15 @@
+package com.homework;
+
+public class ComputerLab extends Room
+{
+
+
+    public ComputerLab(String name, Integer capacity)
+    {
+        super(name, capacity);
+
+    }
+
+
+
+}
