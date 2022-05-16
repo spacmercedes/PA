77aa207ffe3fb54f4859647e6426beb15b31bedc
spacmercedes <79169837+spacmercedes@users.noreply.github.com>
2022-05-16 23:50:26 +0300

    Compulsory10

---
+++ Compulsory10/Laboratorul 10 - Server/src/main/java/ClientThread.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+class ClientThread extends Thread {
+    private Socket socket = null;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            String request = "continue";
+
+            while (request.compareTo("stop") != 0) {
+                // Get the request from the input stream: client → server
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                request = in.readLine();
+
+                // Send the response to the oputput stream: server → client
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                String raspuns = "Server received the request " + request;
+
+                if (request.compareTo("stop") == 0)
+                    raspuns = "Server stopped";
+
+                out.println(raspuns);
+                out.flush();
+            }
+        } catch (IOException e) {
+            System.err.println("Communication error... " + e);
+        } finally {
+            try {
+                socket.close(); // or use try-with-resources
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
